8e64759bec2c21950b024a93e2c92fa89a45ed12
goryny4gor <alekseygor.13@gmail.com>
2013-10-17 00:55:48 +0300

    app use(......)

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var config   = require('config');
 var logger   = require('libs/logger')(module);
 
 var app = express();
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.methodOverride());
+
+if (app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
 
 http.createServer(app).listen(config.get('domain:port'), config.get('domain:host'), function(){
     logger.info('Server started. Host: ' + config.get('domain:port') + '/ Port: ' + config.get('domain:port'));
