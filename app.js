f3fe87128c414a641bd62a2f47d69069b077bef8
goryny4gor <alekseygor.13@gmail.com>
2013-10-17 00:45:33 +0300

    config & logger

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express  = require('express');
 var http     = require('http');
 var path     = require('path');
 var config   = require('config');
+var logger   = require('libs/logger')(module);
 
 var app = express();
 
-http.createServer(app).listen(config.get('domain:port'), config.get('domain:host'));
+http.createServer(app).listen(config.get('domain:port'), config.get('domain:host'), function(){
+    logger.info('Server started. Host: ' + config.get('domain:port') + '/ Port: ' + config.get('domain:port'));
+});
