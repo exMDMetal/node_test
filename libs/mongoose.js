38f9bbc9f2793d0f6be74bc1f83e2323897acd31
ODymnich <alekseygor13@gmail.com>
2013-10-17 11:49:20 +0300

    mongoose conect & error events log

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,5 +1,14 @@
 var mongoose = require('mongoose');
 var config = require('config');
+var logger = require('./logger')(module);
+
+var db = mongoose.connection;
+db.on('open', function(){
+    logger.info('MongoDB started on uri: ' + config.get('mongoose:uri'));
+});
+db.on('error', function(error){
+    logger.error('Failed to connect');
+});
 
 mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'));
 
