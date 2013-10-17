63c88236e39b153afcb0b0a217f19df3f8c84054
ODymnich <alekseygor13@gmail.com>
2013-10-17 12:45:58 +0300

    mongoose conect & error events log

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.favicon());
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, '/static')));
 
 if (app.get('env') == 'development') {
