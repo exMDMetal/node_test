4c4b8cf3bdb7aeb0bb4fe84b972358993ddf573e
ODymnich <alekseygor13@gmail.com>
2013-10-17 17:43:57 +0300

    add test

---
+++ test/places.js
@@ -0,0 +1,21 @@
+var places = [
+    {
+        type1: {
+            category1: {
+
+            },
+            category2: {
+
+            }
+        }
+    },
+    {
+        type2: {
+            category3: {
+
+            }
+        }
+    }
+]
+
+module.exports = places;
