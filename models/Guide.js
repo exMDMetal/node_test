95286c22313d28d5d457c8b69c39b897ab80a507
ODymnich <alekseygor13@gmail.com>
2013-10-18 13:14:02 +0300

    Guide model

--- models/Guide.js
+++ models/Guide.js
@@ -3,7 +3,26 @@ var mongoose = require('libs/mongoose');
 var Schema = mongoose.Schema;
 
 var Guide = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    telephone:{
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
 
+Guide.path('name').validate(function(value){
+    return value > 5 && value < 50;
 });
 
 module.exports = mongoose.model('Guide', Guide);
